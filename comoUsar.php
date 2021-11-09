092078fcca06f9847eb07e1c66f34eb58a2007cc
Valentin Sobrado Carnevali <sobradovalentin@gmail.com>
2021-11-09 11:55:01 -0300

    Desarrolle el archivo PHP, en el cual se explica el funcionamiento de la página de sorteo

---
+++ comoUsar.php
@@ -0,0 +1,26 @@
+<?php
+    echo 
+    '<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sorteos</title>
+    </head>
+    <body>
+        <div>
+        
+                <h2>¿Cómo realizar sorteo?</h2>
+                <p>Para realizar un sorteo, cargar los participantes 
+                    uno por uno, luego de poner su nombre hacer click en el botón "Agregar".
+                    Una vez todos cargados, tocar en SORTEAR y en ese momento va a salir el
+                    ganador por pantalla! </p>
+                <p>También usted tiene las funciones de borrar el ultimo nombre cargado
+                y borrar todos los nombres haciendo click en sus respectivos botónes.</p>
+
+            
+        </div>
+    </body>
+    </html>'
+?>
